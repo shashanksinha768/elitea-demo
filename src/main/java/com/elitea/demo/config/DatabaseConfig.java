28cee58d5b17d869e6a69f4e0e11f8f8d54227df
shashanksinha768 <shashank.sinha768@gmail.com>
2026-07-23 15:05:29 +0530

    Create src/main/java/com/elitea/demo/config/DatabaseConfig.java

---
+++ src/main/java/com/elitea/demo/config/DatabaseConfig.java
@@ -0,0 +1,8 @@
+package com.elitea.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+    // Place database configuration here for production profiles (datasource beans, pooling, etc.)
+}
